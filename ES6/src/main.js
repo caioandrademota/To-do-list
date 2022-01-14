40077c9d64977a2b54681d06d9aa011447452bda
caioandrademota <cam.snf19@uea.edu.br>
2022-01-14 16:48:16 -0400

    inserção dos métodos update, delete e create

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -7,7 +7,11 @@ const apiTodos = new Todos()
 const app = createApp({
     data() {
         return {
-            todos: []
+            todos: [],
+            form: {
+                text: '',
+                done:false
+            }
         }
     },
     created() { 
@@ -17,6 +21,27 @@ const app = createApp({
         async fetchTodos(){
             this.todos = await apiTodos.read();
             console.log(this.todos)
+        },
+        async createTodo() {
+            const data = await apiTodos.create(this.form)
+            this.todos.push(data)
+            this.form.text = ""
+            this.form.done = null
+        },
+        async toggleTodoStatus(todo) {
+            const data = await apiTodos.update({
+                ...todo,
+                done: !todo.done
+            })
+
+            const index = this.todos.findIndex(({ id }) => id === data.id)
+            this.todos[index] = data
+        },
+        async destroyTodo(id) {
+            await apiTodos.delete({ id })
+            
+            const index = this.todos.findIndex((todo) => todo.id === id)
+            this.todos.splice(index,1)
         }
     },
 })
