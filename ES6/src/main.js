46180cf7b9b99ea251cccfa33da316db0271d5c2
caioandrademota <cam.snf19@uea.edu.br>
2022-01-13 22:24:26 -0400

    instalação do vue.js

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -1,3 +1,24 @@
 import Todos from "./api/todos";
 import './assets/css/main.css';
+import { createApp } from "vue";
 
+const apiTodos = new Todos()
+
+const app = createApp({
+    data() {
+        return {
+            todos: []
+        }
+    },
+    created() { 
+        this.fetchTodos()
+    },
+    methods: { 
+        async fetchTodos(){
+            this.todos = await apiTodos.read();
+            console.log(this.todos)
+        }
+    },
+})
+
+app.mount('#app')
